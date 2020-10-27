13b5468867db15068c87e71b8bc056ff75c2aaa7
Coral Solomon <coralsolomon179@gmail.com>
2020-10-27 14:53:59 +0200

    Updates

---
+++ precache-manifest.263384a6fc7a3b5b77e91d3ac9e41cf4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180d33b0a7178eba7a80ca928baa3ff5",
+    "url": "/Coral-Solomon-22-10-2020/index.html"
+  },
+  {
+    "revision": "b00f52d620fc6393c612",
+    "url": "/Coral-Solomon-22-10-2020/static/css/main.44520584.chunk.css"
+  },
+  {
+    "revision": "6b17303a666409fc8ffd",
+    "url": "/Coral-Solomon-22-10-2020/static/js/2.6fbc056e.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/Coral-Solomon-22-10-2020/static/js/2.6fbc056e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00f52d620fc6393c612",
+    "url": "/Coral-Solomon-22-10-2020/static/js/main.77329253.chunk.js"
+  },
+  {
+    "revision": "f72fd6fac6456811380c",
+    "url": "/Coral-Solomon-22-10-2020/static/js/runtime-main.06afeff5.js"
+  },
+  {
+    "revision": "a6e5446a7c5789aabc9b37eaaf72134d",
+    "url": "/Coral-Solomon-22-10-2020/static/media/Oswald-VariableFont_wght.a6e5446a.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/Coral-Solomon-22-10-2020/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "2c7f5dcba99f14fbd7ecf9215c5d3737",
+    "url": "/Coral-Solomon-22-10-2020/static/media/autumn.2c7f5dcb.jpg"
+  },
+  {
+    "revision": "77162ab81cfc8b2af83fe35cfa36034c",
+    "url": "/Coral-Solomon-22-10-2020/static/media/blackHeart.77162ab8.png"
+  },
+  {
+    "revision": "c182f8d9b456e09753595da604b0fdd7",
+    "url": "/Coral-Solomon-22-10-2020/static/media/redHeart.c182f8d9.png"
+  },
+  {
+    "revision": "9d3b8c4d39e1a7bc531cfb8b12b90d68",
+    "url": "/Coral-Solomon-22-10-2020/static/media/search.9d3b8c4d.png"
+  },
+  {
+    "revision": "324c7dad5896d6e3eca91f95d345ed5a",
+    "url": "/Coral-Solomon-22-10-2020/static/media/spring.324c7dad.jpg"
+  },
+  {
+    "revision": "47135622e10f90c135ac7dc2debb7e76",
+    "url": "/Coral-Solomon-22-10-2020/static/media/summer.47135622.jpg"
+  },
+  {
+    "revision": "31dd2e1ea8a41b88a74d2d708c0109a8",
+    "url": "/Coral-Solomon-22-10-2020/static/media/winter.31dd2e1e.jpg"
+  }
+]);
