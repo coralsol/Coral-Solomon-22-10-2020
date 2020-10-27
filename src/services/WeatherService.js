8d07b2946f6ab1d24495fb6ee8638353ce6b1b64
Coral Solomon <coralsolomon179@gmail.com>
2020-10-27 15:00:37 +0200

    weather second commit

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -9,8 +9,8 @@ export default {
 }
 
 async function getWeatherOfTheWeek(location) {
-    let { data } = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=MWtBxp2J0vWMapPa9OKaNck8mxGWosii&q=${location}`)
-    let weather = await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${data[0]['Key']}?apikey=MWtBxp2J0vWMapPa9OKaNck8mxGWosii`)
+    let { data } = await axios.get(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=MWtBxp2J0vWMapPa9OKaNck8mxGWosii&q=${location}`)
+    let weather = await axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${data[0]['Key']}?apikey=MWtBxp2J0vWMapPa9OKaNck8mxGWosii`)
     console.log('weather.data.DailyForecasts',weather.data.DailyForecasts);
     storageService.post(`${location}WeatherOfTheWeek`, weather.data.DailyForecasts)
     return weather.data.DailyForecasts || [];
@@ -18,13 +18,13 @@ async function getWeatherOfTheWeek(location) {
 
 
 async function getWeatherOfTheDay(location) {
-    let { data } = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=MWtBxp2J0vWMapPa9OKaNck8mxGWosii&q=${location}`)
-    let weather = await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${data[0]['Key']}?apikey=MWtBxp2J0vWMapPa9OKaNck8mxGWosii`)
+    let { data } = await axios.get(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=MWtBxp2J0vWMapPa9OKaNck8mxGWosii&q=${location}`)
+    let weather = await axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/1day/${data[0]['Key']}?apikey=MWtBxp2J0vWMapPa9OKaNck8mxGWosii`)
     storageService.post(`${location}WeatherOfTheDay`, weather.data.DailyForecasts)
     return weather.data.DailyForecasts || [];
 }
 
 async function getAutoComplete(location) {
-    let { data } = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=MWtBxp2J0vWMapPa9OKaNck8mxGWosii&q=${location}`)
+    let { data } = await axios.get(`https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=MWtBxp2J0vWMapPa9OKaNck8mxGWosii&q=${location}`)
     return data
 }
